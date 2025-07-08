a53edad897eac6778dfc3ef8334a9329c1eeb646
vrindavmb <vrindamb.official@gmail.com>
2025-07-08 19:27:59 +0530

    fix test and init file

--- test_app.py
+++ test_app.py
@@ -3,6 +3,6 @@ from app import print_name
 
 class TestApp(unittest.TestCase):
     def test_print_name(self):
-        self.assertEqual(print_name(),"Helloworld")
+        self.assertEqual(print_name(),"hello world")
 if __name__=='__main__':
     unittest.main()
